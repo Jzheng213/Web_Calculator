0c171c067d905ead2e7f915817459edc864f5bd9
jzheng <jzheng213@gmail.com>
2016-01-04 22:49:39 -0500

    Calcuator Version 1 Final

--- JavaScript/Web_Calculator.js
+++ JavaScript/Web_Calculator.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    var input = ""
-    var runningNumber = 0
+    'use strict';
+    console.log('JS Started');
+    var input = "";
+    var runningNumber = 0;
     var newEntry = false;
     var currOperation = "";
 
+    
     //numbers
-    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0').click(function(){
-        if(newEntry){$('#answer').val('')};
+    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#decimal').click(function(){
+        if(newEntry){$('#answer').val('');}
+
         input = $(this).text();
         $('#answer').val($('#answer').val() + input);
         newEntry = false;
@@ -14,35 +18,67 @@ $(document).ready(function(){
 
     //clears
     $('#AC').click(function() {
+       console.log('function clear excecuted');
        $('#answer').val('');
        runningNumber = 0;
     });
 
     $('#CE').click(function() {
+        console.log('function clear excecuted');
        $('#answer').val(runningNumber);
+       newEntry = true;
     });
 
     //operations
 
-
-
     $('#plus').click(function() {
+        console.log('function plus excecuted');
         runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
         runningNumber = Number($('#answer').val());
-        currOperation = "+"
+        currOperation = "+";
+        newEntry = true;
+    });
+
+    $('#subtract').click(function() {
+        console.log('function subtract called');
+        runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
+        runningNumber = Number($('#answer').val());
+        currOperation = "-";
         newEntry = true;
     });
 
     $('#multiply').click(function() {
+        console.log('function multiplication called');
         runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
         runningNumber = Number($('#answer').val());
-        currOperation = "x"
+        currOperation = "x";
         newEntry = true;
     });
+
+    $('#divide').click(function() {
+        console.log('function divide called');
+        runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
+        runningNumber = Number($('#answer').val());
+        currOperation = "/";
+        newEntry = true;
+    });
+
+    $('#percent').click(function() {
+        console.log('function percent called');
+        runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
+        runningNumber = Number($('#answer').val());
+        currOperation = "%";
+        $('#equal').trigger('click');
+        newEntry = true;
+    });
+
     //equal
     $('#equal').click(function() {
+        if(currOperation === "="){runningNumber = $('#answer').val();}
+        console.log('function equal called');
         runningNumber = evaluate(currOperation, runningNumber, Number($('#answer').val()));
         $('#answer').val(runningNumber);
+        currOperation = "=";
         newEntry = true;
     });
 
@@ -53,12 +89,20 @@ function evaluate(currOperation, runningNumber, newNumber){
         case "+":
         runningNumber += newNumber;
         return runningNumber;
-        break;
         case "x":
         runningNumber *= newNumber;
         return runningNumber;
-        break;
+        case "-":
+        runningNumber -= newNumber;
+        return runningNumber;
+        case "/":
+        runningNumber /= newNumber;
+        return runningNumber;
+        case "%":
+        runningNumber /= 100;
+        return runningNumber;
+
         default:
         return runningNumber;
-    };
+    }
 }
